010f51ba25afaa9aa77b6fc00ec59511dcbbc4e2
Andaleeb Khogaly <andaleebkhog@gmail.com>
2021-02-09 04:49:23 +0200

    accepting data in a child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+   posts = [
+     {
+       title: 'Neat tree',
+       imageUrl: 'assets/tree.jpeg',
+       userName: 'nature',
+       content: 'Saw this awsoeme tree during my hike today'
+      },
+      {
+        title: 'Mountain biking',
+        imageUrl: 'assets/biking.jpeg',
+        userName: 'bikingmonster',
+        content: 'Check my bike on the mountain, guys!'
+       },
+       {
+        title: 'Snowy mountain',
+        imageUrl: 'assets/mountain.jpeg',
+        userName: 'mountainlover',
+        content: 'Here is a picture of a beautiful mountain i saw today'
+       }
+   ]
 }
